70d0ed7ccdbe2ae397dccbdcd9eb8f76a6068152
Mihail Makei <mihasiknet@gmail.com>
2020-07-24 20:12:28 +0300

    Commit

--- MouseLogger/Source.c
+++ MouseLogger/Source.c
@@ -1,19 +1,20 @@
 #include <ntddk.h>
 
-VOID UnloadDriver(PDRIVER_OBJECT); // Unloads driver and destroys all the devices
-NTSTATUS
+VOID UnloadDriver(PDRIVER_OBJECT);								// Unloads driver and destroys all the devices
+NTSTATUS DispatchPass(PDRIVER_OBJECT, PIRP);
 
 NTSTATUS DriverEntry(
 	PDRIVER_OBJECT	DriverObject,
 	PUNICODE_STRING	RegistryPath
 ) {
-	NTSTATUS status;
-	int i = 0;
+	DriverObject->DriverUnload = UnloadDriver;					// Unload driver
 
+	for (int i = 0; i <= IRP_MJ_MAXIMUM_FUNCTION; ++i)			// Just do nothing with IRP
+		DriverObject->MajorFunction[i] = DispatchPass;
 
-	DriverObject->DriverUnload = UnloadDriver;
+	DriverObject->MajorFunction[IRP_MJ_READ] = DispatchRead;	// Handle the request
 
-	DbgPrint("Hello World!\r\n");
+	DbgPrint("Hello World!\r\n");								// Just hello world
 	
 	return STATUS_SUCCESS;
 }
